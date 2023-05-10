aac81f9d3c20c2d4e5509c5515d227ac45deba3b
reinreingoaway21 <126550731+reinreingoaway21@users.noreply.github.com>
2023-05-10 21:02:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function logcheck(){
+    if(document.getElementById("uname").value === ""){
+        if(document.getElementById("psw").value === ""){
+            
+        document.getElementById("check").innerHTML = "Please enter your username and/or password and/or email"
+    }else{
+        document.getElementById("check").innerHTML = "Please enter your username"
+    }
+}
+if(document.getElementById("uname").value !== ""){
+    if(document.getElementById("psw").value !== ""){
+        document.getElementById("check").innerHTML = "Please enter your  password"
+    } else{
+        ocument.getElementById("check").innerHTML = "Login Success"
+
+    }
+}
+
+}
